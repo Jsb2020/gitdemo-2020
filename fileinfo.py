528726af5c8d59ca5a1e88c7e9a4e6b94d734acf
Jsb2020 <2110538651@qq.com>
2020-03-30 15:26:54 +0800

    fifth

--- fileinfo.py
+++ fileinfo.py
@@ -14,3 +14,18 @@ if sys.version_info >= (3,0):
 
 #第四次修改
 file_name = raw_input("Enter a file name: ")
+
+#第五次修改
+count = 0
+t_char = 0
+try:
+    with open(file_name) as f:
+        line = f.readline()
+        t_char += len(line)
+        while line:
+            count += 1
+            line = f.readline()
+            t_char += len(line)
+except FileNotFoundError as e:
+    print(e)
+    sys.exit()
